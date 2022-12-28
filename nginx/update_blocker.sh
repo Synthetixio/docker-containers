22838c4ac319a4e7e217bd65d0e3481a389b9d79
Jakub Mucha <jakub.mucha@icloud.com>
2022-12-28 21:19:47 +0100

    chore: add comments
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- nginx/update_blocker.sh
+++ nginx/update_blocker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 wget -nv https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/conf.d/globalblacklist.conf -O /etc/nginx/conf.d/globalblacklist.conf
 
-# remove duplicated IPs
+# remove duplicated IPs from globalblacklist.conf
 sed -i 's/^\t*138.199.57.151\t*1;$/#\t138.199.57.151\t\t1;/g' /etc/nginx/conf.d/globalblacklist.conf
 sed -i 's/^\t*143.244.38.129\t*1;$/#\t143.244.38.129\t\t1;/g' /etc/nginx/conf.d/globalblacklist.conf
 sed -i 's/^\t*195.181.163.194\t*1;$/#\t195.181.163.194\t\t1;/g' /etc/nginx/conf.d/globalblacklist.conf
