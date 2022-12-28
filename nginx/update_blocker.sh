dcda7a316942787a1516fcf6e9f1288975719326
Jakub Mucha <jakub.mucha@icloud.com>
2022-12-28 17:41:31 +0100

    feat: add nginx image
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

---
+++ nginx/update_blocker.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget -nv https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/conf.d/globalblacklist.conf -O /etc/nginx/conf.d/globalblacklist.conf
+nginx -t
+service nginx reload
