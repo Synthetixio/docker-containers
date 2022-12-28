0fec184de56325cf933e608bbd4c021da11c6bc3
Jakub Mucha <jakub.mucha@icloud.com>
2022-12-28 21:13:06 +0100

    fix: reloading nginx
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- nginx/update_blocker.sh
+++ nginx/update_blocker.sh
@@ -6,4 +6,4 @@ sed -i 's/^\t*195.181.163.194\t*1;$/#\t195.181.163.194\t\t1;/g' /etc/nginx/conf.
 sed -i 's/^\t*5.188.120.15\t*1;$/#\t5.188.120.15\t\t1;/g' /etc/nginx/conf.d/globalblacklist.conf
 sed -i 's/^\t*89.187.173.66\t*1;$/#\t89.187.173.66\t\t1;/g' /etc/nginx/conf.d/globalblacklist.conf
 nginx -t
-service nginx reload
+nginx -s reload
